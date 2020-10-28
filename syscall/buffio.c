403389a25b67ed16596f60d0015574753cd9c03e
Shravani <shravanipullabotla@gmail.com>
2020-10-28 22:19:28 +0530

    Buffer and I/O demos

---
+++ syscall/buffio.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
+int main(int argc , char const *argv[])
+{
+    printf("Hello\n");
+    printf("world\n");
+    return 0;
+}
