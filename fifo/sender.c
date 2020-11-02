430f28f9663f7c46e78cde269ca69ddbcb27ab63
Shravani <shravanipullabotla@gmail.com>
2020-11-02 17:02:20 +0530

    FIFO Demos

---
+++ fifo/sender.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+//Sender
+int main(int argc,char const *argv[])
+{
+    int fd;
+    fd=open("desd",O_WRONLY);
+    write(fd,"cdac\n",5);
+    close(fd);
+    return 0;
+}
